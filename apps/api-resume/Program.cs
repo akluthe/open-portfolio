a888be3bf1874cc96859094fe22d6c57f5800f36
Andrew Kluthe <andrew@ctech.me>
2025-11-02 19:17:26 -0600

    spitballing some stuff

--- apps/api-resume/Program.cs
+++ apps/api-resume/Program.cs
@@ -1,16 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Add services to the container
 builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 // Add DB connection string from config
 builder.Services.AddSingleton<string>(sp =>
@@ -21,9 +12,17 @@ builder.Services.AddSingleton<string>(sp =>
 // Register ResumeDbService for DI
 builder.Services.AddScoped<IResumeDbService>(sp =>
 {
-    var connString = sp.GetRequiredService<string>();∏
+    var connString = sp.GetRequiredService<string>();
     return new ResumeDbService(connString);
 });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
 // GET /resumes/{slug} using service
 app.MapGet("/resumes/{slug}", async (string slug, IResumeDbService db) =>
